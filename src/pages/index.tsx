9ec62ef41828768a5888dcfe8d1a4332e9313f42
Anna Maria <aannamariabr@outlook.com>
2022-10-07 11:24:03 -0300

    Formatando preço

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,9 +35,9 @@ export default function Home({ products } : HomeProps) {
 
             <footer>
               <strong>{product.name}</strong>
-              <span>R${product.price}</span>
+              <span>{product.price}</span>
             </footer>
-         </Product>
+          </Product>
         )
       })}
     </HomeContainer>
@@ -56,13 +56,17 @@ export const getStaticProps : GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount / 100),
     }
   })
 
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
