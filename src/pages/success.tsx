6d51aa9ad91af490edf6eee06b6b37b0958b07d6
Anna Maria <aannamariabr@outlook.com>
2022-10-07 15:13:58 -0300

    Estrutura visual da página de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SucessContainer } from "../styles/pages/sucess";
+
 export default function Success() { 
   return (
-    <h1>Success</h1>
+    <SucessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+      Uhuul <strong>Diego Fernandes</strong>, sua <strong>camiseta Ignite</strong> já está a caminho da sua casa. 
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SucessContainer>
   )
 }
