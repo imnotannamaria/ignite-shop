4304a1f14e543fa598345f6bc08d1c7ef8bcd365
Anna Maria <aannamariabr@outlook.com>
2022-10-07 11:07:20 -0300

    Configurações inicias do stripe e construção de toda estrutura da página

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from "../styles/pages/app";
+import Image from 'next/future/image'
+
+globalStyles();
+
+export default function MyApp({ Component, pageProps } : AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      
+      <Component {...pageProps} />
+    </Container>
+  )
+}
+
