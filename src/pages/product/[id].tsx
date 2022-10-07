f1b9e1b9477de0df251a7b5a91bf623727e1c2ee
Anna Maria <aannamariabr@outlook.com>
2022-10-07 11:48:01 -0300

    Estilização da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+
+        <span>R$ 61,65</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur neque cupiditate quos illo, quaerat, eius reprehenderit quidem facere hic possimus sapiente vel quo architecto iure! Reiciendis voluptas voluptatum assumenda recusandae.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
